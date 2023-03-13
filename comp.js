15285fd8bd94de07b64199635b9a522a139306c5
Aditya Kumar <aditya201292@gmail.com>
2023-03-13 22:53:09 +0530

    Add UI

--- comp.js
+++ comp.js
@@ -1,3 +1,28 @@
+const button = document.querySelectorAll('#weapon');
+button.forEach((btn)=>{
+    btn.addEventListener('click',playRound)
+})
+
+const compPoints = document.querySelector('.compPoints');
+const youPoints = document.querySelector('.youPoints');
+const declare = document.querySelector('.p2');
+const p1 = document.querySelector('.p1');
+const dialog = document.querySelector('.dialog');
+const dialogP = document.querySelector('.dialogP');
+const newGame = document.querySelector('.newGame');
+const element = document.querySelector('.hidDiv');
+let hidden = element.getAttribute("hidden");
+
+newGame.addEventListener('click', () => {
+    dialog.close();
+})
+
+let result;
+let announce;
+
+let comp=0;
+let you=0;
+
 function getComputerChoice() {
     let ranNum = Math.floor(Math.random()*100);
     let choice;
@@ -13,83 +38,120 @@ function getComputerChoice() {
     return choice;
 }
 
-function playRound (playerSelection, computerSelection) {
-    let player = playerSelection.toLowerCase();
-    let result;
+function playRound (e, computerSelection) {
+    element.setAttribute("hidden", "hidden");
+    let player = e.target.className;
+    computerSelection = getComputerChoice();
+ 
     if(player=="rock"){
         switch (computerSelection) {
             case "rock":
-                result = "Tie!You both chose rock"
+                result = "Tie!";
+                announce = "Both chose Rock";
                 break;
         
             case "paper":
-                result = "You lose!Paper beats Rock"
+                result = "You Lose!";
+                announce = "Paper beats Rock";
                 break;
 
             case "scissor":
-                result = "You won!Rock beats Scissor"
+                result = "You Won!";
+                announce = "Rock beats Scissor";
                 break;
         }
     }else if(player=="paper"){
         switch (computerSelection) {
             case "rock":
-                result = "You won!Paper beats Rock"
+                result = "You Won!";
+                announce = "Paper beats Rock";
                 break;
         
             case "paper":
-                result = "Tie!You both chose Paper"
+                result = "Tie!";
+                announce = "Both chose Paper";
                 break;
 
             case "scissor":
-                result = "You lose!Scissor beats Paper"
+                result = "You Lose!";
+                announce = "Scissor beats Paper";
                 break;
         }
     }else {
         switch (computerSelection) {
             case "rock":
-                result = "You lose!Rock beats Scissor"
+                result = "You Lose!";
+                announce = "Rock beats Scissor";
                 break;
         
             case "paper":
-                result = "You won!Scissor beats Paper"
+                result = "You Won!";
+                announce = "Scissor beats Paper";
                 break;
 
             case "scissor":
-                result = "Tie!You both chose Scissor"
+                result = "Tie!";
+                announce = "Both chose Scissor";
                 break;
         }
     }
-    return result;
+    game(result);
 }
 
-function game() {
-    let comp=0;
-    let you=0;
-    for(let i=0;i<5;i++){
-        let player = prompt("Write what you choose:");
-        let result = playRound(player, getComputerChoice())
-        if(result.search("won")!=-1){
+function game(result) {
+    
+        if(result.search("Won")!=-1){
             ++you;
-            console.log(result);
-        }else if(result.search("lose")!=-1){
+            youPoints.textContent = you;
+            p1.textContent = result;
+            declare.textContent = announce;
+            console.log(`Computer:${comp}  You:${you}`);
+        }else if(result.search("Lose")!=-1){
             ++comp;
-            console.log(result);
+            compPoints.textContent = comp;
+            p1.textContent = result;
+            declare.textContent = announce;
+            console.log(`Computer:${comp}  You:${you}`);
         }
         else{
             ++comp;
             ++you;
-            console.log(result);
+            compPoints.textContent = comp;
+            youPoints.textContent = you;
+            p1.textContent = result;
+            declare.textContent = announce;
+            console.log(`Computer:${comp}  You:${you}`);
         }
-    }
-    if(comp==you){
-        console.log(`It's a Tie computer:${comp} and you:${you}`);
-    }
-    else if(comp>you){
-        console.log(`You lost!computer:${comp} and you:${you}`)
-    }
-    else{
-        console.log(`You won!computer:${comp} and you:${you}`)
-    }
+    if(comp==5||you==5){
+        if(comp==you){
+            dialogP.textContent = "Tie!";
+            element.removeAttribute("hidden");
+            dialog.showModal();
+            comp=0;
+            you=0;
+            compPoints.textContent = comp;
+            youPoints.textContent = you;
+        }
+        else if(comp>you){
+            dialogP.textContent = "You Lost!";
+            element.removeAttribute("hidden");
+            dialog.showModal();
+            comp=0;
+            you=0;
+            compPoints.textContent = comp;
+            youPoints.textContent = you;
+        }
+        else{
+            dialogP.textContent = "You Won!";
+            element.removeAttribute("hidden");
+            dialog.showModal();
+            comp=0;
+            you=0;
+            compPoints.textContent = comp;
+            youPoints.textContent = you;
+        }
+        declare.textContent = "First to score 5 points wins the game";
+        p1.textContent = "Choose Your Weapon";
 }
 
-game();
+    }
